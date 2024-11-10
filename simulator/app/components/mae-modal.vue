200a00b4fe190fc4bd4b117b952a898a70515ca5
KazariEX <1364035137@qq.com>
2024-11-10 18:01:59 +0800

    feat(simulator): 模态框支持

---
+++ simulator/app/components/mae-modal.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+    const modalStore = useModalStore();
+    const { modals } = storeToRefs(modalStore);
+</script>
+
+<template>
+    <template v-for="{ component, zIndex, isOpening } in modals" :key="zIndex">
+        <component :is="component" :is-opening :style="{ zIndex }"/>
+    </template>
+</template>
