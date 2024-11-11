0bea03b9b7175bfe4383a7eb6ea1e6c53ee8bc60
KazariEX <1364035137@qq.com>
2024-11-11 20:23:13 +0800

    feat(core): 异步加载游戏数据

--- packages/core/src/data/index.ts
+++ packages/core/src/data/index.ts
@@ -19,115 +19,61 @@ import type {
     TransformDataTemplate
 } from "./types";
 
-const data = await Promise.all([
-    loadEquipDataStatistics(),
-    loadEquipDataTemplate(),
-    loadShipDataBlueprint(),
-    loadShipDataBreakout(),
-    loadShipDataStatistics(),
-    loadShipDataStrengthen(),
-    loadShipDataTemplate(),
-    loadShipDataTrans(),
-    loadShipMetaBreakout(),
-    loadShipMetaRepair(),
-    loadShipMetaRepairEffect(),
-    loadShipSkinTemplate(),
-    loadShipStrengthenBlueprint(),
-    loadShipStrengthenMeta(),
-    loadSPWeaponDataStatistics(),
-    loadSPWeaponType(),
-    loadTransformDataTemplate()
-]);
-
 export const ShareCfg = {
-    equip_data_statistics: data[0],
-    equip_data_template: data[1],
-    ship_data_blueprint: data[2],
-    ship_data_breakout: data[3],
-    ship_data_statistics: data[4],
-    ship_data_strengthen: data[5],
-    ship_data_template: data[6],
-    ship_data_trans: data[7],
-    ship_meta_breakout: data[8],
-    ship_meta_repair: data[9],
-    ship_meta_repair_effect: data[10],
-    ship_skin_template: data[11],
-    ship_strengthen_blueprint: data[12],
-    ship_strengthen_meta: data[13],
-    spweapon_data_statistics: data[14],
-    spweapon_type: data[15],
-    transform_data_template: data[16]
+    async load() {
+        const datas = await Promise.all([
+            loadData<EquipDataStatistics>("equip_data_statistics"),
+            loadData<EquipDataTemplate>("equip_data_template"),
+            loadData<ShipDataBlueprint>("ship_data_blueprint"),
+            loadData<ShipDataBreakout>("ship_data_breakout"),
+            loadData<ShipDataStatistics>("ship_data_statistics"),
+            loadData<ShipDataStrengthen>("ship_data_strengthen"),
+            loadData<ShipDataTemplate>("ship_data_template"),
+            loadData<ShipDataTrans>("ship_data_trans"),
+            loadData<ShipMetaBreakout>("ship_meta_breakout"),
+            loadData<ShipMetaRepair>("ship_meta_repair"),
+            loadData<ShipMetaRepairEffect>("ship_meta_repair_effect"),
+            loadData<ShipSkinTemplate>("ship_skin_template"),
+            loadData<ShipStrengthenBlueprint>("ship_strengthen_blueprint"),
+            loadData<ShipStrengthenMeta>("ship_strengthen_meta"),
+            loadData<SPWeaponDataStatistics>("spweapon_data_statistics"),
+            loadData<SPWeaponType>("spweapon_type"),
+            loadData<TransformDataTemplate>("transform_data_template")
+        ]);
+        for (const [name, data] of datas) {
+            Object.defineProperty(ShareCfg, name, {
+                value: data,
+                configurable: false,
+                enumerable: true,
+                writable: false
+            });
+        }
+    }
+} as {
+    load: () => Promise<void>;
+    equip_data_statistics: Record<string, EquipDataStatistics>;
+    equip_data_template: Record<string, EquipDataTemplate>;
+    ship_data_blueprint: Record<string, ShipDataBlueprint>;
+    ship_data_breakout: Record<string, ShipDataBreakout>;
+    ship_data_statistics: Record<string, ShipDataStatistics>;
+    ship_data_strengthen: Record<string, ShipDataStrengthen>;
+    ship_data_template: Record<string, ShipDataTemplate>;
+    ship_data_trans: Record<string, ShipDataTrans>;
+    ship_meta_breakout: Record<string, ShipMetaBreakout>;
+    ship_meta_repair: Record<string, ShipMetaRepair>;
+    ship_meta_repair_effect: Record<string, ShipMetaRepairEffect>;
+    ship_skin_template: Record<string, ShipSkinTemplate>;
+    ship_strengthen_blueprint: Record<string, ShipStrengthenBlueprint>;
+    ship_strengthen_meta: Record<string, ShipStrengthenMeta>;
+    spweapon_data_statistics: Record<string, SPWeaponDataStatistics>;
+    spweapon_type: Record<string, SPWeaponType>;
+    transform_data_template: Record<string, TransformDataTemplate>;
 };
-
-function loadEquipDataStatistics() {
-    return loadData<EquipDataStatistics>("equip_data_statistics");
-}
-
-function loadEquipDataTemplate() {
-    return loadData<EquipDataTemplate>("equip_data_template");
-}
-
-function loadShipDataBlueprint() {
-    return loadData<ShipDataBlueprint>("ship_data_blueprint");
-}
-
-function loadShipDataBreakout() {
-    return loadData<ShipDataBreakout>("ship_data_breakout");
-}
-
-function loadShipDataStatistics() {
-    return loadData<ShipDataStatistics>("ship_data_statistics");
-}
-
-function loadShipDataStrengthen() {
-    return loadData<ShipDataStrengthen>("ship_data_strengthen");
-}
-
-function loadShipDataTemplate() {
-    return loadData<ShipDataTemplate>("ship_data_template");
-}
-
-function loadShipDataTrans() {
-    return loadData<ShipDataTrans>("ship_data_trans");
-}
-
-function loadShipMetaBreakout() {
-    return loadData<ShipMetaBreakout>("ship_meta_breakout");
-}
-
-function loadShipMetaRepair() {
-    return loadData<ShipMetaRepair>("ship_meta_repair");
-}
-
-function loadShipMetaRepairEffect() {
-    return loadData<ShipMetaRepairEffect>("ship_meta_repair_effect");
-}
-
-function loadShipSkinTemplate() {
-    return loadData<ShipSkinTemplate>("ship_skin_template");
-}
-
-function loadShipStrengthenBlueprint() {
-    return loadData<ShipStrengthenBlueprint>("ship_strengthen_blueprint");
-}
-
-function loadShipStrengthenMeta() {
-    return loadData<ShipStrengthenMeta>("ship_strengthen_meta");
-}
-
-function loadSPWeaponDataStatistics() {
-    return loadData<SPWeaponDataStatistics>("spweapon_data_statistics");
-}
-
-function loadSPWeaponType() {
-    return loadData<SPWeaponType>("spweapon_type");
-}
-
-function loadTransformDataTemplate() {
-    return loadData<TransformDataTemplate>("transform_data_template");
-}
 
 async function loadData<T>(name: ShareCfgName) {
     const data = await import(`../../../data/ShareCfg(VVVIP)/${name}.json`);
-    return data.default as Record<string, T>;
+    return [
+        name,
+        data.default as Record<string, T>
+    ] as const;
 }
