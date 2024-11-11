34cd86ee44ad7ef4a5a1aa6003c4e3aecb69f181
KazariEX <1364035137@qq.com>
2024-11-12 00:03:52 +0800

    feat(simulator): 展示舰船当前星级

---
+++ simulator/app/components/rarity-star.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+    defineProps<{
+        value?: number;
+        max: number;
+    }>();
+
+    const backgroundStyle = {
+        backgroundImage: `url("/image/prefab/ui/worldui/star_bg.png")`
+    };
+</script>
+
+<template>
+    <ul flex="~">
+        <li
+            v-for="i in max"
+            flex="~"
+            size="13px"
+            m="x--2.5px"
+            :style="backgroundStyle"
+        >
+            <nuxt-img v-if="i <= (value ?? max)" h="full" src="/image/prefab/ui/worldui/star.png"/>
+        </li>
+    </ul>
+</template>
