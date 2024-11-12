83186b82ea455098608bea3d2d6144741a834043
KazariEX <1364035137@qq.com>
2024-11-12 23:53:23 +0800

    fix(simulator): 修复在改造船之间切换时改造项始终绑定第一艘船的问题

--- simulator/app/components/ship/transform-item.vue
+++ simulator/app/components/ship/transform-item.vue
@@ -1,30 +1,28 @@
 <script lang="ts" setup>
     import type { TransformMatrixTemplate } from "maestrale";
 
-    const { templates } = defineProps<{
-        templates: TransformMatrixTemplate[];
-    }>();
-
-    const template = computed(() => {
-        return templates.some(({ edit_trans }) => edit_trans.length)
-            && templates.find(({ enable }) => enable.value)
-            || templates[0];
+    const templates = defineModel<TransformMatrixTemplate[]>({
+        required: true
     });
 
-    const toggleEnabled = template.value && useToggle(template.value.enable);
+    const template = computed(() => {
+        return templates.value.some(({ edit_trans }) => edit_trans.length)
+            && templates.value.find(({ enable }) => enable.value)
+            || templates.value[0];
+    });
 </script>
 
 <template>
     <li
         v-if="template"
-        grid="~ gap-1 justify-items-center"
-        p="y-1"
+        grid="~ gap-1"
+        p="1"
         b="~ solid gray op-40"
         cursor="pointer"
         :class="{
             [`op-50`]: !template.enable.value
         }"
-        @click="toggleEnabled?.()"
+        @click="template.enable.value = !template.enable.value"
     >
         <nuxt-img size="7" :src="`/image/artresource/atlas/modicon/${template.icon}.png`"/>
         <span text="3">{{ template.name }}</span>
