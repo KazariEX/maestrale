07b8ec3975464db792439541528be11b20eaa8e4
KazariEX <1364035137@qq.com>
2024-11-13 01:35:30 +0800

    feat(simulator): 展示可调整的装备和兵装等级

--- simulator/app/components/spweapon/item.vue
+++ simulator/app/components/spweapon/item.vue
@@ -7,6 +7,13 @@
     }>();
     const spweapon = defineModel<SPWeapon | null>();
 
+    function adjustLevel(event: WheelEvent) {
+        if (spweapon.value) {
+            const level = spweapon.value.level.value + (event.deltaY < 0 ? 1 : -1);
+            spweapon.value.level.value = Math.max(0, Math.min(spweapon.value.maxLevel, level));
+        }
+    }
+
     async function select() {
         const id = await selectSPWeapon(shipId, shipType, !!spweapon.value);
         if (id === -1) {
@@ -20,20 +27,39 @@
 
 <template>
     <div
-        overflow="hidden"
+        position="relative"
         size="13"
-        b="rounded-full"
         cursor="pointer"
         @click="select"
+        @wheel="adjustLevel"
     >
         <template v-if="spweapon">
             <rarity-icon
+                b="rounded-full"
                 mode="spweapon"
                 :rarity="spweapon.rarity"
                 :icon="`/image/artresource/atlas/spweapon/${spweapon.icon}.png`"
                 :show-frame="false"
                 padding
             />
+            <label
+                flex="~ gap-1.5px"
+                position="absolute bottom-0 left-0"
+                b="rounded-full"
+                bg="black op-50"
+                font="mono bold"
+                text="3 white"
+                @click.stop
+            >
+                <prime-input-number
+                    input-class="size-5 outline-none bg-transparent text-inherit text-center"
+                    unstyled
+                    :min="0"
+                    :max="spweapon.maxLevel"
+                    :allow-empty="false"
+                    v-model="spweapon.level.value"
+                />
+            </label>
         </template>
     </div>
 </template>
