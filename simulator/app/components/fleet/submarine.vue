3a7b7839dc603aaa68088f54fc088fc14e377d28
KazariEX <1364035137@qq.com>
2024-11-14 03:49:08 +0800

    feat(simulator): 多编队支持

---
+++ simulator/app/components/fleet/submarine.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+    const fleetStore = useFleetStore();
+
+    const options = computed(() => {
+        return fleetStore.submarineFleets.map(({ name }, i) => ({
+            label: name,
+            value: i
+        }));
+    });
+</script>
+
+<template>
+    <fleet-base
+        :options
+        :remove-disabled="fleetStore.submarineFleets.length <= 1"
+        v-model="fleetStore.currentSubmarineIdx"
+        @add="fleetStore.addSubmarineFleet"
+        @remove="fleetStore.removeSubmarineFleet"
+    >
+        <div grid="~ gap-2">
+            <ship-card fleet="submarine" v-model="fleetStore.currentSubmarineFleet.submarine1"/>
+            <ship-card fleet="submarine" v-model="fleetStore.currentSubmarineFleet.submarine2"/>
+            <ship-card fleet="submarine" v-model="fleetStore.currentSubmarineFleet.submarine3"/>
+        </div>
+    </fleet-base>
+</template>
---
+++ simulator/app/components/fleet/surface.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+    const fleetStore = useFleetStore();
+
+    const options = computed(() => {
+        return fleetStore.surfaceFleets.map(({ name }, i) => ({
+            label: name,
+            value: i
+        }));
+    });
+</script>
+
+<template>
+    <fleet-base
+        :options
+        :remove-disabled="fleetStore.surfaceFleets.length <= 1"
+        v-model="fleetStore.currentSurfaceIdx"
+        @add="fleetStore.addSurfaceFleet"
+        @remove="fleetStore.removeSurfaceFleet"
+    >
+        <div grid="~ gap-2">
+            <ship-card fleet="main" v-model="fleetStore.currentSurfaceFleet.main1"/>
+            <ship-card fleet="main" v-model="fleetStore.currentSurfaceFleet.main2"/>
+            <ship-card fleet="main" v-model="fleetStore.currentSurfaceFleet.main3"/>
+        </div>
+        <div grid="~ gap-2">
+            <ship-card fleet="vanguard" v-model="fleetStore.currentSurfaceFleet.vanguard1"/>
+            <ship-card fleet="vanguard" v-model="fleetStore.currentSurfaceFleet.vanguard2"/>
+            <ship-card fleet="vanguard" v-model="fleetStore.currentSurfaceFleet.vanguard3"/>
+        </div>
+    </fleet-base>
+</template>
