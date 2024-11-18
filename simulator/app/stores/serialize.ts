2e702de305172e3a24f30b8786574939de8d9e4d
KazariEX <1364035137@qq.com>
2024-11-18 12:41:39 +0800

    feat(simulator): 为序列化添加防抖

--- simulator/app/stores/serialize.ts
+++ simulator/app/stores/serialize.ts
@@ -9,11 +9,11 @@ export const useSerializeStore = defineStore("serialize", () => {
         mapping: mapping.value
     });
 
-    function serialize(key: string, source: object) {
+    const serialize = useDebounceFn((key: string, source: object) => {
         const data = serializer.serialize(source);
         mapping.value = serializer.mapping;
         localStorage.setItem(key, data);
-    }
+    }, 1000);
 
     function deserialize(key: string) {
         const data = localStorage.getItem(key);
