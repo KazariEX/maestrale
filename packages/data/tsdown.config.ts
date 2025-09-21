e9041740de56e34091fe8d3515c6823367eb584e
KazariEX <zinkawaii@qq.com>
2025-09-21 18:32:58 +0800

    ci: 添加发布工作流

--- packages/data/tsdown.config.ts
+++ packages/data/tsdown.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig([{
-    entry: [
-        "./src/index.ts",
-    ],
+    exports: {
+        devExports: true,
+    },
 }, {
     entry: {
         postinstall: "./src/scripts/index.ts",
