f558ae041988627d5cb32f48ce179a9585f8984e
KazariEX <zinkawaii@qq.com>
2025-12-03 02:45:51 +0800

    chore: 检测缺失的指挥喵素材

--- scripts/assets.ts
+++ scripts/assets.ts
@@ -1,7 +1,7 @@
 import { existsSync } from "node:fs";
 import { join, resolve } from "node:path";
 import consola from "consola";
-import { createEquip, createShip, createSPWeapon, Equip, ShareCfg, Ship, SPWeapon, useTechnology } from "../packages/core/src";
+import { Commander, createCommander, createEquip, createShip, createSPWeapon, Equip, ShareCfg, Ship, SPWeapon, useTechnology } from "../packages/core/src";
 
 interface Item {
     asset: string;
@@ -9,78 +9,48 @@ interface Item {
 }
 
 const baseDir = resolve(import.meta.dirname, "../simulator/public/assets/artresource/atlas");
-
 const technology = useTechnology();
-
 await ShareCfg.load();
-checkShip();
-checkEquip();
-checkSPWeapon();
 
-function checkShip() {
-    const items: Item[] = [];
-    for (const id of Ship.ids) {
-        const ship = createShip(id, { technology });
-        if (!ship) {
-            continue;
-        }
-        const asset = ship.painting.value;
+check("ship", "squareicon", () => Ship.ids.map((id) => {
+    const ship = createShip(id, { technology })!;
+    return {
+        asset: ship.painting.value,
+        name: ship.name.value,
+    };
+}));
 
-        const path = join(baseDir, "squareicon", asset + ".png");
-        if (!existsSync(path)) {
-            items.push({
-                asset,
-                name: ship.name.value,
-            });
-        }
+check("equip", "equips", () => Equip.ids.map((id) => {
+    const equip = createEquip(id)!;
+    return {
+        asset: equip.icon,
+        name: equip.name,
+    };
+}));
+
+check("spweapon", "spweapon", () => SPWeapon.ids.map((id) => {
+    const weapon = createSPWeapon(id)!;
+    return {
+        asset: weapon.icon,
+        name: weapon.name,
+    };
+}));
+
+check("commander", "commandericon", () => Commander.ids.map((id) => {
+    const commander = createCommander(id)!;
+    return {
+        asset: commander.painting,
+        name: commander.originalName,
+    };
+}));
+
+function check(name: string, folder: string, getter: () => Item[]) {
+    const items = getter().filter(({ asset }) => {
+        const path = join(baseDir, folder, asset + ".png");
+        return !existsSync(path);
+    });
+    if (items.length) {
+        consola.info(`Missing ${name}:`);
+        console.table(items.toSorted((a, b) => a.asset.localeCompare(b.asset)));
     }
-    print("ships", items);
-}
-
-function checkEquip() {
-    const items: Item[] = [];
-    for (const id of Equip.ids) {
-        const equip = createEquip(id);
-        if (!equip) {
-            continue;
-        }
-        const asset = equip.icon;
-
-        const path = join(baseDir, "equips", asset + ".png");
-        if (!existsSync(path)) {
-            items.push({
-                asset,
-                name: equip.name,
-            });
-        }
-    }
-    print("equips", items);
-}
-
-function checkSPWeapon() {
-    const items: Item[] = [];
-    for (const id of SPWeapon.ids) {
-        const weapon = createSPWeapon(id);
-        if (!weapon) {
-            continue;
-        }
-        const asset = weapon.icon;
-
-        const path = join(baseDir, "spweapon", asset + ".png");
-        if (!existsSync(path)) {
-            items.push({
-                asset,
-                name: weapon.name,
-            });
-        }
-    }
-    print("spweapons", items);
-}
-
-function print(name: string, items: Item[]) {
-    if (!items.length) {
-        return;
-    }
-    consola.info(`Missing ${name}:`);
-    console.table(items.toSorted((a, b) => a.asset.localeCompare(b.asset)));
 }
